95e45e497f7ef10f41e0e19a97c04039e3eb4ff2
evgenii shiltsev <e.shiltsev@gmail.com>
2020-08-26 09:23:26 +0300

    Initial Commit

---
+++ toDOlist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  toDOlist
+//
+//  Created by Evgenii Shiltsev on 26.8.2020.
+//  Copyright © 2020 Evgenii Shiltsev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
